03eb8cabd68a513c261df9f4681f505bd3257472
xFizzy <64964282+xFizzy@users.noreply.github.com>
2020-09-17 23:50:55 +0200

    Discord js polska

---
+++ commands/komendy/testembed.js
@@ -0,0 +1,14 @@
+const Discord = require(`discord.js`);
+const { MessageEmbed } = require(`discord.js`);
+
+module.exports = {
+    name: "testembed",
+    run: async (bot, message, args) => {
+
+        const wiadomosc_embed = new Discord.MessageEmbed()
+        .setTitle(`xFizzy nakuka js`)
+   	
+        .setThumbnail(message.author.avatarURL())
+        message.channel.send(wiadomosc_embed)
+    }
+}
